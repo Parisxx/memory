6072490653bf6085a3558e78cfb4a973113a34ec
Parisxx <505825@vistacollege.nl>
2022-06-13 10:41:48 +0200

    logo

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,23 +9,24 @@
     <link rel="stylesheet" href="styles.css">
     <link rel="icon" href="media/icon.png" type="image/gif" sizes="16x16">
 </head>
+
 <body>
 
-<a href="index.php">
- <img class= "logo" src="media/benderal.png">
-</a>
+    <img class="logo" src="media/benderal.png">
 
-<div class="card">
-<div class="card-main">
-</div>
 
-<div class="card-side">
-</div>
-</div>
+    <div class="card">
+        <div class="card-main">
+        </div>
 
- <img class= "logo2" src="media/wordart.png">
-</a>
-<img class= "logo3" src="media/namewordart.png">
-</a>
+        <div class="card-side">
+        </div>
+    </div>
+
+    <img class="logo2" src="media/wordart.png">
+
+    <img class="logo3" src="media/namewordart.png">
+
 </body>
+
 </html>
