d19d46c23842f3aba38ad5b89c999f6503065923
= <=>
2022-06-15 11:12:49 +0200

    dfg

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
         </div>
 
         <div class="card-side">
-        <img class="cards_blank" src="media/blank_200.png">
+        <img class="cards_blank" src="media/blank_150.png">
         </div>
     </div>
 
