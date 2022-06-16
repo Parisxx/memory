815e42f9f9bd95fed003489fd51c7b033abc92fb
Parisxx <505825@vistacollege.nl>
2022-06-16 13:25:49 +0200

    javascript

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>vista.benderniet | Mémoire</title>
     <link rel="stylesheet" href="styles.css">
-    <script src="javascript.js" charset="utf-8"></script>
+    
     <link rel="icon" href="media/icon.png" type="image/gif" sizes="16x16">
 </head>
 
@@ -52,6 +52,9 @@
     <img class="logo2" src="media/wordart.png">
 
     <img class="logo3" src="media/namewordart.png">
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="javascript.js" charset="utf-8"></script>
 
 </body>
 
