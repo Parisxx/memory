de7d54ee431cbab8cff0337b458215b0710ac3c9
= <=>
2022-06-20 13:14:04 +0200

    javascript

--- javascript.js
+++ javascript.js
@@ -2,10 +2,31 @@ let cards = document.getElementsByClassName('cards_blank');
 
 // game state to check how many cards we have selected
 let state = 0;
+let images = [
+    'nando_150.png',
+    'jerbo_150.png',
+    'luc_150.png',
+    'merlijn_150.png',
+    'paris_150.png',
+    'robin_150.png',
+    'vigo_150.png',
+    'yorrick_150.png',
+    'yorrick_150.png',
+    'nando_150.png',
+    'jerbo_150.png',
+    'luc_150.png',
+    'merlijn_150.png',
+    'paris_150.png',
+    'robin_150.png',
+    'vigo_150.png',
+    'yorrick_150.png',
+    'yorrick_150.png',
+]
 
 for(let i = 0; i < cards.length; i++) {
 
     let card = cards[i];
+
 
     card.addEventListener('click', () => {
         
@@ -14,20 +35,29 @@ for(let i = 0; i < cards.length; i++) {
         if(state == 2) {
 
             // check if cards match
-
             //   if else class match 
            
             // if they dont match flip them.
-
+            $('.active').css({'background-image': ''});
             $('.active').removeClass('active');
-
+           
             state = 0;
         } else {
             card.classList.toggle('active');
+            card.style.backgroundImage = 'url(./media/' + images[i] + ')'
             state++;
-        } 
-        
-       console.log(state)
+        }
+
+        if(state == 2) {
+            let activeClasses = $('.active');
+            if(activeClasses[0].style.backgroundImage == activeClasses[1].style.backgroundImage) {
+                // puntentelling hier doen
+
+                
+                activeClasses[0].classList.remove('active');
+                activeClasses[1].classList.remove('active');
+            }
+        }
         
     });
 }
