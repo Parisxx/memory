1648ffc436fcab0a322d0268630f384eeea57a77
= <=>
2022-06-21 11:29:51 +0200

    fix

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>vista.benderniet | Mémoire</title>
+    <title>vista.benderniet | Memoire</title>
     <link rel="stylesheet" href="styles.css">
-    
+
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 
     <link rel="icon" href="media/icon.png" type="image/gif" sizes="16x16">
