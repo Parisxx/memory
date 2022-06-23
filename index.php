5668e10144e95a1e23bafdcf33adf79c60e2c267
Parisxx <505825@vistacollege.nl>
2022-06-23 14:57:11 +0200

    last fix

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>vista.benderniet | Memoire</title>
+    <title>vista.benderniet | Memory</title>
     <link rel="stylesheet" href="styles.css">
     <link rel="icon" href="media/icon.png" type="image/gif" sizes="16x16">
 
